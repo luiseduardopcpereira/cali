3b580af35eb2e47ce6c92544a1a947ad100a5b53
Hand <handmartins@gmail.com>
2021-01-18 20:00:23 -0300

    Ajuste para o envio de email

---
+++ cali/envia_fale.php
@@ -0,0 +1,30 @@
+<?
+
+# alterar email 
+
+$destinatario = "handmartins@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$telefone = $_REQUEST['telefone'];
+$assunto = $_REQUEST['assunto'];
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "telefone: " . $telefone . "\n";
+$body = $body . "assunto: " . $assunto . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:obrigado.htm");
+
+
+?>
